f33c46431f6d5a1a25a84353f48359770db82fb3
Murad R. Alizada <muradral@code.local>
2023-01-19 16:03:08 +0400

    01.19.23

---
+++ script.js
@@ -0,0 +1,35 @@
+// let number = parseInt(prompt("Please enter valid number"));
+// while (number > i) {   
+// number /= 2;
+// }
+// if (number == i) {   
+// console.log("Quvvetidir");
+// } else {
+// console.log("Quvveti deyil");
+// }
+
+// function Change(number) {
+//     let copy = number;
+//     let remainder = number % i0;
+//     let leftSide = (number - remainder) / i0;
+//     while (number > i0) {
+//         number /= i0;
+//           remainder*=i0;
+//     }
+//     let result = remainder + leftSide;
+//     console.log(copy);
+//     console.log(result);
+// }
+//      Change(2435);
+
+// function ArrayGenerator(num1,num2){
+//     let arr = []
+//     let index =0
+//     for (let i = num1 + 1; i < num2; i++) {
+//        arr[index++] = i;
+//     }
+//     return arr;
+// }
+// for (const number of ArrayGenerator(200,300)){
+//   console.log(number);
+// }
